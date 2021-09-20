76950009886b36acbda379ef818847cb95ba86e1
Chaokai Lai <chaokai.lai@gmail.com>
2021-09-20 14:05:35 +1000

    feat: add cli welcome interface

--- day01/vue-auto-router-cli/bin/kkb.js
+++ day01/vue-auto-router-cli/bin/kkb.js
@@ -1,3 +1,10 @@
 #!/usr/bin/env node
-console.log("hello cli......")
 // npm i commander download-git-repo ora handlebars figlet clear chalk open -s
+const program = require('commander')
+
+
+program.version(require('../package.json').version)
+program.command('init <name>').description('init project').action(name => {
+  require('../lib/init')(name)
+})
+program.parse(process.argv)
