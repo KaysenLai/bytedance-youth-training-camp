0a51ae869bcc0c201e93a597162b2ee480483689
Chaokai Lai <chaokai.lai@gmail.com>
2021-09-20 15:40:51 +1000

    feat: add downloading progress bar

--- day01/vue-auto-router-cli/lib/download.js
+++ day01/vue-auto-router-cli/lib/download.js
@@ -2,9 +2,9 @@ const {promisify} = require('util')
 
 module.exports.clone = async function(repo, desc) {
   const download = promisify(require('download-git-repo'))
-  // const ora = require('ora')
-  // const process = ora(`downloading...${repo}`)
-  // await process.start()
+  const ora = require('ora')
+  const process = ora(`downloading...${repo}`)
+  await process.start()
   await download(repo, desc)
-  // process.succeed()
+  process.succeed()
 }
