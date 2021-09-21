7a608879802cdcb6c7d019b06ae28d0ccb5580d7
Chaokai Lai <chaokai.lai@gmail.com>
2021-09-21 17:56:47 +1000

    fix: add esbuild concept

--- day02/learn-vite/mini-vite/src/main.js
+++ day02/learn-vite/mini-vite/src/main.js
@@ -1,2 +1,9 @@
 console.log("main")
 import {createApp} from "vue";
+
+// node_modules
+// esbuild => go => 打包速度很快
+// esbuild => 一个文件  =>  只返回一个 文件 浏览器就能理解了
+
+// lodash-es => 有n个import => http请求过多了，浏览器吃不消
+// lodash-es 有n个import => 使用 esbuild 变成一个 Import
