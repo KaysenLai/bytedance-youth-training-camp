5fbdee16d6d6747880ebf3449168ec3f0333b9f1
Chaokai Lai <chaokai.lai@gmail.com>
2021-09-21 16:48:29 +1000

    feat: rollup external dependency

--- day02/learn-rollup/src/index.js
+++ day02/learn-rollup/src/index.js
@@ -2,8 +2,9 @@ import foo from "./foo"
 import bar from "./bar"
 import {age} from "./assets/data.json";
 import answer  from "the-answer";
-// import {cre} from 'vue'
+import {createApp} from 'vue'
 
 console.log(foo)
 console.log(age)
 console.log(answer)
+console.log(createApp())
